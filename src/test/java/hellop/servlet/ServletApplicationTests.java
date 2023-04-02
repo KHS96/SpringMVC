de1f08e107c32c23e6a75e6181ec1a47bf1cc548
KHS96 <7heesu7@hanmail.net>
2023-04-02 14:32:27 +0900

    commit

---
+++ src/test/java/hellop/servlet/ServletApplicationTests.java
@@ -0,0 +1,13 @@
+package hellop.servlet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
