43282e17d821fd428d9e8a7df41bcf9e1d4f443a
KHS96 <7heesu7@hanmail.net>
2023-04-25 17:11:08 +0900

    response test

---
+++ src/main/java/hellop/servlet/basic/response/ResponseHtmlServlet.java
@@ -0,0 +1,26 @@
+package hellop.servlet.basic.response;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "responseHtmlServlet" , urlPatterns = "/response-html")
+public class ResponseHtmlServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Content-Type : text/html;charset=utf-8
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<body>");
+        writer.println("    <div>Hello</div>");
+        writer.println("</body>");
+        writer.println("</html>");
+    }
+}
---
+++ src/main/java/hellop/servlet/basic/response/ResponseJsonServlet.java
@@ -0,0 +1,34 @@
+package hellop.servlet.basic.response;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hellop.servlet.basic.HelloData;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name="responseJsonServlet" , urlPatterns = "/response-json")
+public class ResponseJsonServlet extends HttpServlet {
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("utf-8");
+
+        HelloData helloData = new HelloData();
+        helloData.setUsername("kim");
+        helloData.setAge(20);
+
+        //{"username":"kim","age" : 20
+        String result = objectMapper.writeValueAsString(helloData);
+
+        response.getWriter().write(result);
+
+
+    }
+}
